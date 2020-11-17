e8e6b5c7313a626688c84b481a5407e09e0f30f5
udayaditya singh <udayaditya.singh@gmail.com>
2020-11-18 04:50:49 +0530

    "changes"

--- test/test.js
+++ test/test.js
@@ -7,9 +7,9 @@ describe('node-app', () => {
     })
 
     describe('GET /', function() {
-        it('respond with hello udaynikki', (done) => {
+        it('respond with hello nikki', (done) => {
             //navigate to root and check the response is "hello world"
-            request(app).get('/').expect('hello udaynikki').end(done);
+            request(app).get('/').expect('hello nikki').end(done);
         });
     });
 })
